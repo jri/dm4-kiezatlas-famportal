1e4bab6145ca86e1fe01d270d15a2f52be758620
Jörg Richter <jri@deepamehta.de>
2014-03-18 17:57:27 +0100

    Create Famportal test categories and render tree

--- src/main/resources/web/script/app.js
+++ src/main/resources/web/script/app.js
@@ -1,5 +1,12 @@
 function SearchController($scope, $http) {
 
+    var FAMPORTAL_CATEGORY_ROOT = "famportal.category.root"
+
+    $http.get("/core/topic/by_value/uri/" + FAMPORTAL_CATEGORY_ROOT).success(function(data) {
+        console.log("root", data)
+        $scope.root = data
+    })
+
     $scope.search = function() {
         console.log("search", $scope.searchTerm)
         $http.get("/core/topic?search=*" + $scope.searchTerm + "*&field=ka2.criteria.angebot").success(function(data) {
