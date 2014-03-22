2cb8e0ca1be20755b88cda4978d3b7ef64399082
Jörg Richter <jri@deepamehta.de>
2014-03-22 14:37:03 +0100

    Use Angular routes

---
+++ src/main/resources/web/script/famportal.js
@@ -0,0 +1,54 @@
+angular.module("famportal", ["ngRoute"])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when("/editorial", {
+                templateUrl: "/de.kiezatlas.famportal/partials/editorial.html",  controller: "famportalController"
+            })
+            .when("/categories", {
+                templateUrl: "/de.kiezatlas.famportal/partials/categories.html", controller: "searchController"
+            })
+            .otherwise({redirectTo: "/editorial"})
+    })
+    .controller("famportalController", function($scope, famportalService) {
+
+        famportalService.getFamportalCategories(function(categories) {
+            $scope.root = categories
+        })
+
+        $scope.showGeoObjects = function(famportalCategoryId) {
+            famportalService.getGeoObjectsByCategory(famportalCategoryId, function(geoObjects) {
+                $scope.geoObjects = geoObjects
+            })
+        }
+    })
+    .controller("searchController", function($scope, famportalService) {
+        $scope.search = function() {
+            console.log("search", $scope.searchTerm)
+            famportalService.searchAngebotsArten($scope.searchTerm, function(categories) {
+                $scope.categories = categories
+            })
+        }
+    })
+    .service("famportalService", function($http) {
+        var FAMPORTAL_CATEGORY_ROOT = "famportal.category.root"
+
+        this.getFamportalCategories = function(callback) {
+            $http.get("/core/topic/by_value/uri/" + FAMPORTAL_CATEGORY_ROOT).success(function(data) {
+                console.log("root", data)
+                callback(data)
+            })
+        }
+
+        this.getGeoObjectsByCategory = function(famportalCategoryId, callback) {
+            $http.get("/famportal/geoobject/category/" + famportalCategoryId).success(function(data) {
+                console.log("geoobjects for category", famportalCategoryId, data)
+                callback(data.items)
+            })
+        }
+
+        this.searchAngebotsArten = function(searchTerm, callback) {
+            $http.get("/core/topic?search=*" + searchTerm + "*&field=ka2.criteria.angebot").success(function(data) {
+                callback(data)
+            })
+        }
+    })
